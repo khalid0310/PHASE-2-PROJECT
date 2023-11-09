0f4f20c93c0c128761614e74c29e2e2d8a93c30e
khalid <khalid.mohamed@moringaschool.com>
2023-11-09 17:53:33 +0300

    creating card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css'
 function App() {
   return (
-    
+    <>
+      <Main/>
+    </>
   );
 }
 
